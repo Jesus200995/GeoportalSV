b382ed4edbbd8b096ca8ccd7b12509cd4f097c11
Jesus200995 <jesus.riosgomez@utxicotepec.edu.mx>
2025-05-28 12:09:49 -0600

    PERFECTO

--- geoportal/src/views/UploadLayer.vue
+++ geoportal/src/views/UploadLayer.vue
@@ -677,22 +677,31 @@ const generateMockLayers = () => {
                  class="mt-4 p-4 rounded-lg" 
                  :class="{
                    'bg-green-50 border-l-4 border-green-500': uploadStatus === 'success',
+                   'bg-blue-50 border-l-4 border-blue-500': uploadStatus === 'uploading',
                    'bg-yellow-50 border-l-4 border-yellow-500': uploadStatus === 'warning',
                    'bg-red-50 border-l-4 border-red-500': uploadStatus === 'error'
                  }"
             >
               <div class="flex items-start">
+                <!-- Icono de éxito -->
                 <svg v-if="uploadStatus === 'success'" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-500 mt-0.5 mr-2 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
+                <!-- Icono de subida en progreso -->
+                <svg v-else-if="uploadStatus === 'uploading'" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-500 mt-0.5 mr-2 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clip-rule="evenodd" />
+                </svg>
+                <!-- Icono de advertencia -->
                 <svg v-else-if="uploadStatus === 'warning'" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500 mt-0.5 mr-2 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
                 </svg>
+                <!-- Icono de error -->
                 <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-500 mt-0.5 mr-2 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                 </svg>
                 <span :class="{
                   'text-green-700': uploadStatus === 'success',
+                  'text-blue-700': uploadStatus === 'uploading',
                   'text-yellow-700': uploadStatus === 'warning',
                   'text-red-700': uploadStatus === 'error'
                 }">{{ statusMessage }}</span>
